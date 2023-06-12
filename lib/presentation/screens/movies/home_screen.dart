2cafdee61b6f065cadbbd2ebc6b39e83b708c980
Jonathan Valverde <jonathanval19@hotmail.com>
2023-06-12 18:23:51 -0500

    HumanFormats

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -38,7 +38,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
 
-    //final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
 
     final slideShowMovies = ref.watch( moviesSlideshowProvider );
 
@@ -48,7 +48,13 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
         const CustomAppbar(),
 
-        MoviesSlideshow(movies: slideShowMovies)
+        MoviesSlideshow(movies: slideShowMovies),
+
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subTitle: 'Lunes 12',
+        )
 
       ],
     );
